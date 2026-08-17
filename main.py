788df3fbee8e7312d76246f06702a56ba2dcf3b6
Juan Gonzalez <juangonzalez@Juans-MacBook-Air.local>
2026-08-17 17:34:41 -0300

    job search

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
-from typing import List
-
-from pydantic import BaseModel, Field
+from unittest import result
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-
-load_dotenv()
 from langchain.agents import create_agent
+from langchain_tavily import TavilySearch
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
+from tavily import TavilyClient
 
+load_dotenv()
 
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
+tavily = TavilyClient()
+#llm = ChatOpenAI(model="gpt-4o")
 
-    url: str = Field(description="The URL of the source")
+#prompt = ChatPromptTemplate.from_template("What is the capital of {country}?")
 
-
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
-
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
-
-
-llm = ChatOpenAI(model="gpt-5")
-tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
-
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: search query
+    Returns:
+        str: search results
+    """
+    print(f"Searching for {query} ")
+    #return "tokyo weather is sunny"
+    return tavily.search(query=query)
+    
+llm = ChatOpenAI(model="gpt-4o-mini")
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+    #result = agent.run("What is the weather in Tokyo?")
+    #print(result)
 
 def main():
-    print("Hello from langchain-course!")
-    result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
-        }
-    )
-    print(result)
-
+        print("Hello from langchain-course!")
+        result = agent.invoke({"messages":HumanMessage(content="busca 3 trabajos que busquen un Ingeniero IA / Automation Engineer en LinkedIn que solicite habilidades como N8N o Make. Y listame detalles de cada uno. Busco en España pero de forma remota.") })
+        print(result)
 
 if __name__ == "__main__":
     main()
